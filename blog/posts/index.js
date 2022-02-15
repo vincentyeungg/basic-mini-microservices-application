1e5fe0fe0ed0d5fe096bdab6a995bb6571c82046
vincentyeungg <vincentyeung0809@gmail.com>
2022-02-15 01:17:20 -0500

    added skaffold config file to automate k8s tasks

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -22,7 +22,7 @@ app.get("/posts", (req, res) => {
 });
 
 // route to add a post
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
